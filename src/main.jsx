b634b13120d67b5b42fd16da05872e75e38bf1f8
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2025-04-13 23:07:20 +0100

    custom component added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.jsx'
-import StarRating from './StarRating.jsx'
+import StarRating from "./StarRating.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
-    <StarRating maxRating={10} />
-    <StarRating />
-  </StrictMode>,
-)
+    <StarRating maxRating={5} />
+  </StrictMode>
+);
