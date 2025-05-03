7d643c137bcea13c5474a2324803c65c00f181c7
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2025-05-03 16:54:38 +0100

    adding useRef to focus inputs

--- src/components/Navbar/NavbarInput.jsx
+++ src/components/Navbar/NavbarInput.jsx
@@ -1,24 +1,30 @@
 import { useEffect, useRef } from "react";
 
-
 function NavbarInput({ query, setQuery }) {
-  const inputEl = useRef(null)
-    
-  useEffect(function(){
-    inputEl.current.focus()
-    console.log(inputEl.current);
-    
-    
-  }, [])
-    return (
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        ref={inputEl}
-      />
-    );
-  }
-  export default NavbarInput;
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    function callback(e) {
+
+      if(document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("Keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
+    />
+  );
+}
+export default NavbarInput;
